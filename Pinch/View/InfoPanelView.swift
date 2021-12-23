000ed9b710e979f6c8041132d29043eb2e174ae9
Simon Rofe <simon.rofe@ieee.org>
2021-12-23 16:27:56 +1000

    Add metrics to the info panel view

--- Pinch/View/InfoPanelView.swift
+++ Pinch/View/InfoPanelView.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 
 struct InfoPanelView: View {
+    var scale: CGFloat
+    var offset: CGSize
+
     var body: some View {
         HStack {
             Image(systemName: "circle.circle")
                 .symbolRenderingMode(.hierarchical)
                 .resizable()
                 .frame(width: 30, height: 30)
+            Spacer()
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                Spacer()
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                Spacer()
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+            }
         }
     }
 }
 
 struct InfoPanelView_Previews: PreviewProvider {
     static var previews: some View {
-        InfoPanelView()
+        InfoPanelView(scale: 1, offset: .zero)
             .preferredColorScheme(.dark)
             .previewLayout(.sizeThatFits)
             .padding()
