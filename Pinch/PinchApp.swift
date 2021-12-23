20c663e1728143dd13ef6dfcce3218087f4b21d8
Simon Rofe <simon.rofe@ieee.org>
2021-12-23 12:00:13 +1000

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Simon Rofe on 23/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
