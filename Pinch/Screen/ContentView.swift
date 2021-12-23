dae9e5daa1429c8e23a33ba4bbfc75c65e5d2405
Simon Rofe <simon.rofe@ieee.org>
2021-12-23 12:47:09 +1000

    Add image to main screen

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Image("magazine-front-cover")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(10)
+                .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+            }
+            .navigationBarTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
