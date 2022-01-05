de5db51d72e8de33bfb69b562d719980ebfcd695
Simon Rofe <simon.rofe@ieee.org>
2022-01-05 16:22:03 +1000

    Add background to button control interface

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -74,6 +74,10 @@ struct ContentView: View {
                             ControlImageView(icon:  "plus.magnifyingglass")
                         }
                     }
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
                 }
                     .padding()
                 , alignment: .bottom
